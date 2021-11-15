77603a5c6f1d86139867a82237754b1c01d63045
SURBHI SHEKHAR <79759981+ssurbhi09@users.noreply.github.com>
2021-11-15 12:24:04 +0530

    Add files via upload

---
+++ Conditional and loop/Equal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Enter two numbers" << endl;
+
+	cin >> a >> b;
+	if (a == b) {
+		cout << "Hey these are equal" << endl;
+		
+		}
+		}
