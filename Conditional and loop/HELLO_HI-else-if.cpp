9b5da5b6c3df3caf7e38aa68b96e3b1526383251
SURBHI SHEKHAR <79759981+ssurbhi09@users.noreply.github.com>
2021-11-15 12:30:23 +0530

    Add files via upload

---
+++ Conditional and loop/HELLO_HI-else-if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=10,b=20,c=100;
+	if(a<=b||c<=b){
+		cout<<"hello"<<endl;
+		}
+		else if (a<=b||a<=c){
+			cout<<"hi"<<endl;
+		}
+		else{
+			cout<<"hey";
+		}
+}
