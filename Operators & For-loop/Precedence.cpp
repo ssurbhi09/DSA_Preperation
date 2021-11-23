e53a79777033569cc85534d45082e7b110cd741e
SURBHI SHEKHAR <79759981+ssurbhi09@users.noreply.github.com>
2021-11-23 18:34:54 +0530

    Add files via upload

---
+++ Operators & For-loop/Precedence.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a =2 * 3 + 4;
+	int b =10 * 5 / 9;
+	cout<< a<< endl;
+	cout<< b<< endl;
+}
---
+++ Operators & For-loop/Print 1 to N .cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main (){
+	int n ,m;
+	cout<<"Enter m & n"<<endl;
+	cin >> m>> n;
+	
+	for (;m <=n ; m++){
+		cout<< m<<endl;
+	}
+}
