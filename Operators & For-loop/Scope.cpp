6b7a07a11581794e77573f3521c410ab9267a0fc
SURBHI SHEKHAR <79759981+ssurbhi09@users.noreply.github.com>
2021-12-05 13:59:36 +0530

    Add files via upload

---
+++ Operators & For-loop/Scope.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i=10;
+	
+	//not allowed int i =22;
+	cout<<i<<endl;
+	
+	if (i==10){
+		int i =23;
+		cout<< i<< endl;
+	}
+	cout<< i<< endl;
+	
+	for (int k=0; k<10;k++){
+		cout<< k<< endl;
+	}
+}
